6b194ca7c6a357923c17fc2dd597819901225f7b
errornotjoin <joshuahastings29@gmail.com>
2026-02-26 13:10:08 +0000

    feat(start_scan): implement scan selection form and output display; add CSS for layout styling

--- Drive_add_and_scan/start_scan.php
+++ Drive_add_and_scan/start_scan.php
@@ -17,6 +17,7 @@ if(!isset($_SESSION['username']) or !isset($_SESSION['ID']) or !isset($_SESSION[
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <link rel="stylesheet" href="../css/Only_background.css">
+    <link rel="stylesheet" href="../css/start_scan.css">
     <title>add Drive -- Home Server Interface</title>
 </head>
 <body>
@@ -35,39 +36,84 @@ if(!isset($_SESSION['username']) or !isset($_SESSION['ID']) or !isset($_SESSION[
     ?>
     <main>
     <section class="add_drive_section">
-        <form action="#" method="post">
+        <form action="start_scan.php" method="GET" class="Pick_the_scan">
             <select name="Type_of_scans">
                 <?php
+                #empty list 
                 $list= [];
+                #getting the types of scans from the yml file and putting them in a list
                 $Type_of_scan = array_keys($other_yml_link['types_of_scans']);
+                #placing the names into options for the select tag
                 foreach($Type_of_scan as $scan){
-                    echo "<option value='". $scan ."'>". $scan ."</option>";
+                    if(isset($_GET['Type_of_scans']) == true)
+                    {
+                        if($scan == $_GET['Type_of_scans'])
+                        {
+                            echo "<option value='". $scan ."' selected>". $scan ."</option>";
+                        }
+                        else
+                        {
+                            echo "<option value='". $scan ."'>". $scan ."</option>";
+                        }
+                    }
+                    else
+                    {
+                        echo "<option value='". $scan ."'>". $scan ."</option>";
+                    }
                 }
                 ?>
             </select>
             <input type="submit" value="Scan Drive">
         </form>
-        <section>
+        <section class="the_out_put">
             <?php
-                $Type_of_scan = array_keys($other_yml_link['types_of_scans']);
-                
-                foreach($Type_of_scan as $scan)
+                if(isset($_GET['Type_of_scans']))
                 {
-                    $other_scan = $other_yml_link['types_of_scans'][$scan]['booledan'];
+                    #regetting the items from the yml files
+                    $Type_of_scan = array_keys($other_yml_link['types_of_scans']);
                     
-                    
-                        if( $scan == $_POST['Type_of_scans'] ){
-                            $other_yml_link['types_of_scans'][$scan]['booledan'] = "True";
-                        }
-                        else{
-                            $other_yml_link['types_of_scans'][$scan]['booledan'] = "False";
-                        }
-                        file_put_contents($Other_paths, Yaml::dump($other_yml_link, 4, 2));
-                        print_r($other_scan);
-                    
+                
+                    foreach($Type_of_scan as $scan)
+                    {   #getting the value of the booledan for the current scan
+                        $other_scan = $other_yml_link['types_of_scans'][$scan]['booledan'];
+                        
+                            #checking if scan == User picked scan
+                            if( $scan == $_GET['Type_of_scans'] )
+                            {
+                                #change the vaule to true if it is the same and false if it is not
+                                $other_yml_link['types_of_scans'][$scan]['booledan'] = "True";
+                            }
+                            else
+                            {
+                                #change the vaule to false
+                                $other_yml_link['types_of_scans'][$scan]['booledan'] = "False";
+                            }
+                            file_put_contents($Other_paths, Yaml::dump($other_yml_link, 4, 2));
+                            #print_r($other_scan);
+                        
+                    }
+                    set_time_limit(0);
+                    #cd = where the python folder is e.g it will open the command prompt in that folder and then run the python executable from the venv and then run the index.py file with the -u flag to make it unbuffered so we can get the output in real time and 2>&1 is to get the error output as well
+                    #the second part is this folder python executable is
+                    #the last part is the python file to run and the -u is to make it unbuffered so we can get the output in real time
+                    #and 2>&1 is to get the error output as well
+                    $cmd = 'cd C:\xampp\htdocs\home_server_information && C:\xampp\htdocs\home_server_interface\.venv\Scripts\python.exe -u index.py 2>&1';
+                    #opens a process to run the command and get the output
+                    $handle = popen($cmd, "r");
+                    #check if its at the end of file and if it's not then get the output and print it
+                    while (!feof($handle)) {
+                        #print the output of the command
+                        $buffer = fgets($handle);
+                        #covents it to html friendly format and prints it
+                        echo "<pre><u>" . htmlspecialchars($buffer) . "</pre>";
+                        #flushes the output buffer to the browser
+                        ob_flush();
+                        flush();
+                    }
+                    pclose($handle);
+
                 }
-                $output = shell_exec( "C:\\Windows\\System32\\cmd.exe /c c:\\xampp\\htdocs\\home_server_interface\\.venv\\Scripts\\python.exe C:\\xampp\\htdocs\\home_server_information\\index.py 2>&1");    
-                echo "<pre>$output</pre>";
+                
             ?>
         
         </section>
