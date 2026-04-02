b54685ee3933437003854e12a4c8df0724a9ffce
errornotjoin <joshuahastings29@gmail.com>
2026-04-02 17:09:13 +0100

    feat: add YML file viewer functionality; implement drive and scan type display sections

--- yml_file_viewer.php
+++ yml_file_viewer.php
@@ -1,7 +1,9 @@
 <?php
+
+use Symfony\Component\Yaml\Yaml;
 include "outside_links.php";
 include "database/sql_login.php";
-
+require 'vendor/autoload.php'; 
 session_start();
 if(!isset($_SESSION['username']) or !isset($_SESSION['ID']) or !isset($_SESSION['Level'])){
     header("Location: index.php?error=Not_logged_in");
@@ -17,6 +19,7 @@ if(!isset($_SESSION['username']) or !isset($_SESSION['ID']) or !isset($_SESSION[
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/home.css">
     <link rel="stylesheet" href="css/background.css">
+    <link rel="stylesheet" href="css/Yml_file_viewer.css">
     <title>YML File Viewer -- Home Server Interface</title>
 </head>
 <body>
@@ -32,6 +35,35 @@ if(!isset($_SESSION['username']) or !isset($_SESSION['ID']) or !isset($_SESSION[
         </div>
     </header>
     <main>
+        <section class="folders_section">
+            <div class="header_of_folders">
+                <h2>Drives </h2>
+            </div>
+            <div class="Main_folders">
+                <?php
+
+                    $drive_name = Yaml::parseFile($paht);
+                    $drive_name = $drive_name['drives'];
+                    foreach($drive_name as $drive){
+                        echo "<p>". $drive ."</p>";
+                    }   
+                ?>
+            </div>
+        </section >
+        <section class="folders_section">
+            <div class="header_of_folders">
+                <h2>Scans Types </h2>
+            </div>
+            <div class="Main_folders">
+                <?php
+                    $Type_of_scan = array_keys($other_yml_link['types_of_scans']);
+                    foreach($Type_of_scan as $scan){
+                            $new_scan = str_replace("_", " ", $scan);
+                            echo "<p>". $new_scan ."</p>";
+                    }
+                ?>
+            </div>
+        </section>
         
     </main>
     
